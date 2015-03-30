c23e9ef38385c8f0e220b03d684bc43074b33c0e
Martin Bagge <brother@bsnet.se>
2015-03-30 13:49:20 +0200

    FEATURE: Add columnexists() check for DB upgrade script.
    When trying to add a extra column that is already present a fatal error
    will be presented instead of a silent pass.
    Add a new funciton to db/functions.php that take a table name and a
    column name and returns true if the queried combination exists. Useful
    in upgrading when you do not want to add a already present column name.

--- db/functions.php
+++ db/functions.php
@@ -20,4 +20,24 @@ function tableexists ($name)
 
 }
 
+function columnexists ($table, $column)
+{
+    $DBH = new DB();
+    $query= "
+        SELECT table_schema, table_name, column_name
+        FROM information_schema.columns
+        WHERE table_schema='".$DBH->getDBName()."'
+            AND table_name='adminuser'
+            AND column_name='deleted';
+            ";
+
+    $DBH->query($query);
+    $DBH->execute();
+    if ($DBH->rowCount() == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 ?>
