1e0188ea41ef7ffe3baa3851612610390d8584ee
Martin Bagge <brother@bsnet.se>
2015-04-10 22:03:39 +0200

    FEATURE: Enforce utf8 encoding in PDO communication.

--- public_html/PDO.interface.class.php
+++ public_html/PDO.interface.class.php
@@ -51,6 +51,7 @@ class DB {
         $dsn = 'mysql:host=' . $this->_host . ';dbname=' . $this->_dbname;
 
         $options = array(
+                         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
                          PDO::ATTR_PERSISTENT	=> true,
                          PDO::ATTR_ERRMODE	=> PDO::ERRMODE_EXCEPTION
         );
